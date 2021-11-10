aed9368583f98a088e8961857b633d72fe9092b2
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-10 16:47:11 -0700

    done and done

--- index.test.js
+++ index.test.js
@@ -119,6 +119,12 @@ describe('[Exercise 6] Car', () => {
 })
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
-  test('[19] resolves true if passed an even number', () => {})
-  test('[20] resolves false if passed an odd number', () => {})
+  test('[19] resolves true if passed an even number', async () => {
+ const res = await utils.isEvenNumberAsync(2)
+ expect(res).toBe(true)
+  })
+  test('[20] resolves false if passed an odd number', async () => {
+    const res = await utils.isEvenNumberAsync(1)
+ expect(res).toBe(false)
+  })
 })
